1a16db2d0ae778f16a66366bb45275a8be2f9f1a
hemfic <ericreesor@outlook.com>
2017-11-16 21:08:04 -0500

    Base scripts and base scene

---
+++ PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+using System.IO.Ports;
+
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	private Rigidbody rb;
+	Vector3 origin;
+	void Start ()
+	{	rb = GetComponent<Rigidbody>();
+		origin = rb.position;
+	}
+
+	void reset(){
+		rb.position=origin;
+	}
+
+	void FixedUpdate (){
+		float r = Input.GetAxis ("Cancel");
+		if(r!=0.0f){
+			reset();
+		}
+		string[] ports = SerialPort.GetPortNames();
+		for (int i=0;i<ports.Length;i++){
+			print (ports[i]);
+		}
+	}
+
+
+		
+}
