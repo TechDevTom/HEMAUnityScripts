d90b4f8ea0c355e9e04b2395ba5dc7074a77b90e
Conlan LaFreniere <conlanlafreniere@cmail.carleton.ca>
2018-03-26 13:59:11 -0400

    Movement Evaluation Update
    - Added updated diagrams
    - Implemented rudimentary movement evaluation functionality, built from
    the rotation and position checks (no timeliness evaluation)

--- Scripts/MovementReader.cs
+++ Scripts/MovementReader.cs
@@ -39,6 +39,9 @@ public class MovementReader : MonoBehaviour {
     private Queue<Transform> steadyChecker;
     private float startTime;
 
+    private float finalGrade;
+    private int totalMovements;
+
     private int steadyFrameCount;
     private int steadyDegreeVariation;
     private int steadyPositionVariation;
@@ -48,8 +51,10 @@ public class MovementReader : MonoBehaviour {
     void Start () {
         movement = createMovementFromFile(PlayerPrefs.GetString("name"));
         records = movement.movement;
+        totalMovements = records.Count;
         started = false;
         finished = false;
+        finalGrade = 0;
         steadyChecker = new Queue<Transform>();
 
         checkSword = new GameObject();
@@ -118,7 +123,7 @@ public class MovementReader : MonoBehaviour {
                     {
                         finished = true;
                         Text txt = label.GetComponent<Text>();
-                        txt.text = "Good Job!";
+                        txt.text = "Good Job!\n Accuracy : " + getLetterGrade(finalGrade / totalMovements);
                         return;
                     }
                     else
@@ -140,11 +145,13 @@ public class MovementReader : MonoBehaviour {
                     {
                         //valid check within time interval, so remove the current transform
                         currentRecord = null;
+                        finalGrade += getAccuracy(checkSword.transform, sword.transform);
                     }
                 }
                 else if ((t - startTime) > currentRecord.time + (movement.binSize / 2))
                 {
                     //failed movement check
+
                     started = false;
 
                     //reset movement checking queue
@@ -155,6 +162,7 @@ public class MovementReader : MonoBehaviour {
 
                     indicatorSword.transform.position = movement.start.position;
                     indicatorSword.transform.rotation = movement.start.rotation;
+                    
                 }
             }
             
@@ -162,6 +170,66 @@ public class MovementReader : MonoBehaviour {
         }
 
 	}
+
+    string getLetterGrade(float grade)
+    {
+        if(grade < 0.5)
+        {
+            return "F";
+        } else if (grade < 0.5 + (1/24))
+        {
+            return "D-";
+        }
+        else if (grade < 0.5 + (2 / 24))
+        {
+            return "D";
+        }
+        else if (grade < 0.5 + (3 / 24))
+        {
+            return "D+";
+        }
+        else if (grade < 0.5 + (4 / 24))
+        {
+            return "C-";
+        }
+        else if (grade < 0.5 + (5 / 24))
+        {
+            return "C";
+        }
+        else if (grade < 0.5 + (6 / 24))
+        {
+            return "C+";
+        }
+        else if (grade < 0.5 + (7 / 24))
+        {
+            return "B-";
+        }
+        else if (grade < 0.5 + (8 / 24))
+        {
+            return "B";
+        }
+        else if (grade < 0.5 + (9 / 24))
+        {
+            return "B+";
+        }
+        else if (grade < 0.5 + (10 / 24))
+        {
+            return "A-";
+        }
+        else if (grade < 0.5 + (11 / 24))
+        {
+            return "A";
+        }
+        return "A+";
+    }
+
+    float getAccuracy(Transform t1, Transform t2)
+    {
+        float ret = 0;
+        ret += 1-(Quaternion.Angle(t1.rotation, t2.rotation) / steadyDegreeVariation);
+        ret += 1-(Vector3.Distance(t1.position, t2.position) / steadyPositionVariation);
+        return ret / 2;
+    }
 
     bool areTransformsSimilar(Transform t1, Transform t2)
     {
